b9d74d67b65dbedda792646df25b09db9ab27e2f
alwos19 <jhon.jaramilloe@udea.edu.co>
2024-12-18 10:29:44 -0500

    First Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+function Greeting(){
+    const married = false
+    return <h1>{ married ? 'Estoy casado' : 'No estoy casado'}</h1>
+}
+
+root.render(<Greeting />)
